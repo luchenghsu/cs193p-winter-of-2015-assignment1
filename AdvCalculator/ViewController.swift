dccbf7ad80b167b9ab35fe062b64a5a59760d30a
luchenghsu <luchenghsu@gmail.com>
2015-02-14 22:33:35 -0800

    Set the initial value of secondDisplay.text ""

--- AdvCalculator/ViewController.swift
+++ AdvCalculator/ViewController.swift
@@ -19,6 +19,18 @@ class ViewController: UIViewController {
 
 
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        secondDisplay.text = ""
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
     @IBAction func appendDigit(sender: UIButton) {
         
         let digit = sender.currentTitle!
