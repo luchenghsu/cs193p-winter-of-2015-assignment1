91a068a1ee18e987c5ae045ef9e221792f97f36d
luchenghsu <luchenghsu@gmail.com>
2015-02-16 02:04:06 -0800

    Change the Calculator to MVC version for Assignment 2 - initial

--- AdvCalculator/CalculatorBrain.swift
+++ AdvCalculator/CalculatorBrain.swift
@@ -10,4 +10,105 @@ import Foundation
 
 class CalculatorBrain {
     
+    private var opStack = [Op]()
+    private var knownOps = [String:Op]()
+    
+    private enum Op: Printable {
+        case Operand(Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        case UnaryOperation(String, Double -> Double)
+        case ConstantOperation(String, () -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                case .ConstantOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    init() {
+        
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("×", { $0 * $1 }))
+        learnOp(Op.BinaryOperation("÷", { $1 / $0 }))
+        learnOp(Op.BinaryOperation("+", { $0 + $1 }))
+        learnOp(Op.BinaryOperation("−", { $1 - $0 }))
+        learnOp(Op.UnaryOperation("√", { sqrt($0) }))
+        learnOp(Op.UnaryOperation("sin", { sin($0) }))
+        learnOp(Op.UnaryOperation("cos", { cos($0) }))
+        learnOp(Op.ConstantOperation("π", { M_PI }))
+    }
+    
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+    
+            switch op {
+                
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+            case .ConstantOperation(_, let operation):
+                
+                return (operation(), remainingOps)
+    
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+              
+            case .BinaryOperation(_, let operation):
+        
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                            return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                        
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double)-> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        println("symbol=\(symbol)")
+        if let operation = knownOps[symbol] {
+            println("perform Operation inside if let operation=")
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    func clear() {
+        opStack.removeAll(keepCapacity: false)
+    }
+    
 }
--- AdvCalculator/ViewController.swift
+++ AdvCalculator/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     //var userIsInTheMiddleOfTypingANumber: Bool = false
     
     var userIsInTheMiddleOfTypingANumber = false
+    var brain = CalculatorBrain()
 
 
     
@@ -61,63 +62,35 @@ class ViewController: UIViewController {
     
     @IBAction func clear(sender: UIButton) {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.removeAll(keepCapacity: false)
+        brain.clear()
         display.text = "0"
         history.text = ""
     }
     
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         
-        history.text = history.text! + operation
-        
-        switch operation {
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $0 / $1 }
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $0 - $1 }
-        case "√": performOperation { sqrt($0) }
-        case "sin": performOperation { sin($0)}
-        case "cos": performOperation { cos($0) }
-        case "π": performOperation { M_PI }
-            
-        default: break
+        if let operation = sender.currentTitle {
+            history.text = history.text! + operation
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
             
         }
         
     }
     
-    
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: () -> Double) {
-        displayValue = operation()
-        enter()
-    }
-    
-    // var operandStack: Array<Double> = Array<Double>()
-    var operandStack = Array<Double>()
-
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
         history.text = history.text! + "\(displayValue)"
     }
     
